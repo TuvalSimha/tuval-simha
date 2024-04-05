e7ee4774021f6c09637eb92f77c6df9f7b4ca79f
TuvalSimha <tuval.simha@gmail.com>
2024-04-05 19:06:55 +0300

    Prettier check

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,6 +3,8 @@ import { Button } from "./button";
 
 export function Hero() {
   const router = useRouter();
+
+
   return (
     <section>
       <div className="relative items-center w-full px-5 py-12 mx-auto md:px-12 lg:px-16 max-w-7xl lg:py-24">
