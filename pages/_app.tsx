d2ccceb7544882259ac421c042ee81e67f1958b5
TuvalSimha <tuval.simha@gmail.com>
2024-04-08 11:34:46 +0300

    SpeedInsights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import "../styles/globals.css"
 import { Analytics } from "@vercel/analytics/react"
 import { Layout } from "../components/layout"
 import Head from "next/head"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
   return (
@@ -18,6 +19,7 @@ export default function App({ Component, pageProps }: AppProps): ReactElement {
       </Head>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </Layout>
   )
 }
